55bbe05df04c2482d740b17a9d90d4a656b6c580
Felix-012 <f.nuetzel@gmx.de>
2024-05-15 15:40:19 +0200

    adjusted train_dataloader

--- training/train_radbert_lora.py
+++ training/train_radbert_lora.py
@@ -216,7 +216,7 @@ def main():
 
     # Scheduler and math around the number of training steps.
     overrode_max_train_steps = False
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+    num_update_steps_per_epoch = math.ceil(len(train_dataset) / args.gradient_accumulation_steps)
     if args.max_train_steps is None:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
         overrode_max_train_steps = True
@@ -229,13 +229,13 @@ def main():
     )
 
     # Prepare everything with our `accelerator`.
-    unet, optimizer, train_dataloader, lr_scheduler, noise_scheduler, vae, text_encoder = accelerator.prepare(
-        [unet, optimizer, train_dataloader, lr_scheduler, noise_scheduler, vae, text_encoder]
+    unet, optimizer, lr_scheduler, noise_scheduler, vae, text_encoder = accelerator.prepare(
+        [unet, optimizer, lr_scheduler, noise_scheduler, vae, text_encoder]
     )
 
 
     # We need to recalculate our total training steps as the size of the training dataloader may have changed.
-    num_update_steps_per_epoch = math.ceil(len(train_dataloader) / args.gradient_accumulation_steps)
+    num_update_steps_per_epoch = math.ceil(len(train_dataset) / args.gradient_accumulation_steps)
     if overrode_max_train_steps:
         args.max_train_steps = args.num_train_epochs * num_update_steps_per_epoch
     # Afterwards we recalculate our number of training epochs
