38c9dcd9ab786fc87b7a663ccd32a010eb6f0c4b
Felix-012 <f.nuetzel@gmx.de>
2024-05-22 19:23:55 +0200

    update cp

--- util_scripts/utils_train.py
+++ util_scripts/utils_train.py
@@ -277,7 +277,7 @@ def get_parser_arguments_train(parser):
     parser.add_argument(
         "--resume_from_checkpoint",
         type=str,
-        default=None,
+        default="latest",
         help=(
             "Whether training should be resumed from a previous checkpoint. Use a path saved by"
             ' `--checkpointing_steps`, or `"latest"` to automatically select the last available checkpoint.'
